92d20911d29b9567fe5cacdcaa8a155b1d6ceec4
vir2alwhale <matej.marjanovich@gmail.com>
2014-07-21 17:47:13 +0200

    Step 5, AsyncTask

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -23,23 +24,8 @@ public class MainListActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
         
-        try{
-        	URL blogFeedURL = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
-        	HttpURLConnection connection = (HttpURLConnection) blogFeedURL.openConnection();
-        	connection.connect();
-        	
-        	int responseCode = connection.getResponseCode();
-        	Log.i(TAG, "Code: " + responseCode);
-        }
-        catch(MalformedURLException e){
-        	Log.e(TAG, "Exception caught: ");
-        }
-        catch(IOException e){
-        	Log.e(TAG, "Exception caught: ");
-        }
-        catch(Exception e){
-        	Log.e(TAG, "Exception caught: ");
-        }
+        GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+        getBlogPostsTask.execute();
         
         //Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
     }
@@ -50,6 +36,35 @@ public class MainListActivity extends ListActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_list, menu);
         return true;
+    }
+    
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+		@Override
+		protected String doInBackground(Object... params) {
+			int responseCode = -1;
+			
+			try{
+	        	URL blogFeedURL = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+	        	HttpURLConnection connection = (HttpURLConnection) blogFeedURL.openConnection();
+	        	connection.connect();
+	        	
+	        	responseCode = connection.getResponseCode();
+	        	Log.i(TAG, "Code: " + responseCode);
+	        }
+	        catch(MalformedURLException e){
+	        	Log.e(TAG, "Exception caught: ");
+	        }
+	        catch(IOException e){
+	        	Log.e(TAG, "Exception caught: ");
+	        }
+	        catch(Exception e){
+	        	Log.e(TAG, "Exception caught: ");
+	        }
+			
+			return "Code: " + responseCode;
+		}
+    	
     }
 
     @Override
