4eaebdba4b982b2026b100a3fc52a68a577a935f
vir2alwhale <matej.marjanovich@gmail.com>
2014-07-31 14:41:18 +0200

    BaBRA app
    Testing creating a local repo to upload to github

--- src/com/example/blogreader/MainListActivity.java
+++ src/com/example/blogreader/MainListActivity.java
@@ -192,7 +192,7 @@ public class MainListActivity extends ListActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_share) {
             return true;
         }
         return super.onOptionsItemSelected(item);
